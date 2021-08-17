982381b455f41e8ea0cfbb156ab428fa912c6049
Erik Hayrelov <erik_man.united@abv.bg>
2021-08-18 01:51:59 +0300

    Scaffold Register

---
+++ Web/DiamondBet.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DiamondBet.Data;
+using DiamondBet.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DiamondBet.Web.Areas.Identity.IdentityHostingStartup))]
+namespace DiamondBet.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
