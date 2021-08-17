d8ae77bb0c515398988f441bd8b5fb5e6cb585be
Erik Hayrelov <erik_man.united@abv.bg>
2021-08-18 02:25:00 +0300

    Add UserCoinsViewComponent

---
+++ Web/DiamondBet.Web.ViewModels/ViewComponents/UserCoinsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DiamondBet.Web.ViewModels.ViewComponents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class UserCoinsViewModel
+    {
+        public decimal Coins { get; set; }
+    }
+}
---
+++ Web/DiamondBet.Web/ViewComponents/UserCoinsViewComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace DiamondBet.Web.ViewComponents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using DiamondBet.Data.Models;
+    using DiamondBet.Web.ViewModels.ViewComponents;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserCoinsViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserCoinsViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke(ClaimsPrincipal userClaim)
+        {
+            var user = this.userManager.GetUserAsync(userClaim);
+
+            var coins = user.Result.Coins;
+
+            var model = new UserCoinsViewModel
+            {
+                Coins = coins,
+            };
+
+            return this.View(model);
+        }
+    }
+}
